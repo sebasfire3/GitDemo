4c113d8a27c70008e408257a5589178b5ca0a915
firef <firefox3beta@hotmail.com>
2020-06-08 20:22:13 -0500

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira");
+		
+	}
+	@Test
+	public void deleteTwittter()
+	{
+		
+		System.out.println("deleteTwittter");
+		
+	}
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");
+		
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		
+		System.out.println("elementsUI");
+	}
+}
